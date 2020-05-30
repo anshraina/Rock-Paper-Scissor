2e51c792ce29f74b9eb8a0f6b37b21e42938ef54
anshraina <anshraina13@gmail.com>
2020-05-31 00:18:55 +0530

    Implemented the game only animation remaining

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const game = ()=>{
-    let playerScore =0;
-    let computerScore = 0;
+    let pScore =0;
+    let cScore = 0;
     
     const startGame = ()=>{
         const playBtn = document.querySelector(".intro button");
@@ -9,11 +9,93 @@ const game = ()=>{
 
         playBtn.addEventListener("click", ()=>{
             introScreen.classList.add("fadeOut");
-            
             matchScreen.classList.add("fadeIn");
         });
     };
+
+    //start match
+    const playMatch = ()=>{
+        const options = document.querySelectorAll(".option button");
+        const playerHand = document.querySelector(".player-hand");
+        const computerHand = document.querySelector(".computer-hand");
+        const computerOption = ['rock', 'paper', 'scissor'];
+       
+        options.forEach((option)=>{
+            option.addEventListener("click", function(){
+                const compNumber = Math.floor(Math.random() * 3);
+                const compChoice = computerOption[compNumber];
+                console.log(compChoice);
+
+                // call compareHands
+                compareHands(this.textContent, compChoice);
+                
+                playerHand.src = `./assets/${this.textContent}.png`;
+                computerHand.src = `./assets/${compChoice}.png`;
+            });
+        });
+    }
+
+    const updateScore = ()=>{
+        const playerScore = document.querySelector(".player-score p");
+        const compScore = document.querySelector(".computer-score p");
+        playerScore.textContent = pScore;
+        compScore.textContent = cScore;
+    }
+    const compareHands = (playerChoice, compChoice)=>{
+        const winner = document.querySelector(".winner");
+        if(playerChoice === compChoice){
+            winner.textContent = "Its a tie";
+            return;
+        }
+        //rock
+        if(playerChoice === 'rock'){
+            if(compChoice === 'scissor'){
+                winner.textContent = 'Player Wins';
+                pScore++;
+                updateScore();
+                return;
+            }
+            else{
+                winner.textContent = "Computer Wins";
+                cScore++;
+                updateScore();
+                return;
+            }
+           
+        }
+        if(playerChoice === 'paper'){
+            if(compChoice === 'rock'){
+                winner.textContent = 'Player Wins';
+                pScore++;
+                updateScore();
+                return;
+            }
+            else{
+                winner.textContent = "Computer Wins";
+                cScore++;
+                updateScore();
+                return;
+            }
+            
+        }
+        if(playerChoice === 'scissor'){
+            if(compChoice === 'paper'){
+                winner.textContent = 'Player Wins';
+                pScore++;
+                updateScore();
+                return;
+            }
+            else{
+                winner.textContent = "Computer Wins";
+                cScore++;
+                updateScore();
+                return;
+            }
+           
+        }
+    }
     startGame();
+    playMatch();
 };
 
 //Start
